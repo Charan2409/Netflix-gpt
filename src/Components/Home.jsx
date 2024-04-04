3a2045d817397dfb15691d588af353979fbd01ee
Charan2409 <bandisaicharan111@gmail.com>
2024-04-04 20:00:22 +0530

    committing initial code

---
+++ src/Components/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>  
+        <div>
+            <h1 className='text-red-600 text-2xl font-bold'>Netflix</h1>
+            
+        </div>
+
+    </>
+  )
+}
+
+export default Home
